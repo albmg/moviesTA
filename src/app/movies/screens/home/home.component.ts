29296cba55cc66fe010d4f38cd45516bd3731503
albmg <albmguer@gmail.com>
2022-10-10 01:02:45 +0100

    add landing page

--- src/app/movies/screens/home/home.component.ts
+++ src/app/movies/screens/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  goToPopularMovies() {
+    this.router.navigateByUrl('/popular')
+  }
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
